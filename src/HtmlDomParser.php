276b36cf7443cf95faa82115accec7521afebdad
coolswitcher <CoolSwitcher@gmail.com>
2019-11-12 16:42:35 +0300

    version loader

--- src/HtmlDomParser.php
+++ src/HtmlDomParser.php
@@ -2,19 +2,29 @@
 
 namespace AppZz\Helpers;
 
-require __DIR__.DIRECTORY_SEPARATOR.'SimpleHtmlDom_1_8'.DIRECTORY_SEPARATOR.'simple_html_dom.php';
-
 class HtmlDomParser {
 
-    public static $version = '1_8';
+    private static $_version;
+
+    public static function load_version ($version)
+    {
+        HtmlDomParser::$_version = str_replace (array('.', ','), array('_', '_'), $version);
+        require_once __DIR__.DIRECTORY_SEPARATOR.'SimpleHtmlDom_'.HtmlDomParser::$_version.DIRECTORY_SEPARATOR.'simple_html_dom.php';
+    }
 
 	public static function file_get_html()
 	{
-		return call_user_func_array ('\SimpleHtmlDom_'.HtmlDomParser::$version.'\file_get_html', func_get_args());
+        if (empty(HtmlDomParser::$_version)) {
+            HtmlDomParser::load_version ('1.8');
+        }
+        return call_user_func_array ('\SimpleHtmlDom_'.HtmlDomParser::$_version.'\file_get_html', func_get_args());
 	}
 
 	public static function str_get_html()
 	{
-		return call_user_func_array ('\SimpleHtmlDom_'.HtmlDomParser::$version.'\str_get_html' , func_get_args());
+        if (empty(HtmlDomParser::$_version)) {
+            HtmlDomParser::load_version ('1.8');
+        }
+        return call_user_func_array ('\SimpleHtmlDom_'.HtmlDomParser::$_version.'\str_get_html' , func_get_args());
 	}
 }
